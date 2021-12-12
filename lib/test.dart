83009c3334af1df6af05c0cfde79885a8b8103bd
siriporn nabangchang <nabangchang_s2@silpakorn.edu>
2021-12-13 00:20:40 +0700

    Initial commit

---
+++ lib/test.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  var i = 0;
+  var r = Random();
+  var answer = r.nextInt(100);
+  int count = 0;
+  print('╔════════════════════════════════');
+  print('║       GUESS THE NUMBER');
+  print('╚════════════════════════════════');
+  while (true) {
+    stdout.write('GUESS the number between 1 and 100: ');
+    var input = stdin.readLineSync();
+    var guess = int.tryParse(input!);
+    if (guess != null) {
+      if (guess == answer) {
+        count++;
+        print('║ ➜ '+guess.toString() +'is CORRECT ❤, total guesses: '+count.toString());
+        print('╟──────────────────────────────── ');
+        print('║       THE END');
+        print('╚════════════════════════════════');
+        break;
+      } else if (guess < answer) {
+        print('║ ➜ '+guess.toString() +'is TOO LOW! ▼');
+        print('╟──────────────────────────────── ');
+        count++;
+      } else if (guess > answer) {
+        print('║ ➜ '+guess.toString() +'is TOO HIGH! ▲');
+        print('╟──────────────────────────────── ');
+        count++;
+      }
+    }
+  }
+}
